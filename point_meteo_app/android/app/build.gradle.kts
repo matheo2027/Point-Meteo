769b2ea35738bb323220e4e53693b0765fb54418
matheo2027 <matheo.piques@epitech.eu>
2026-05-20 19:32:12 +0200

    [TRY FIX]

--- point_meteo_app/android/app/build.gradle.kts
+++ point_meteo_app/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.point_meteo_app"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 37
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
